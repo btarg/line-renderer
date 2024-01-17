11fada4e320c4963c4d8d403848e1e41d8091d82
betalars <contact@betalars.de>
2024-01-17 22:18:59 +0100

    fixing typo in declaration

--- addons/LineRenderer/line_plugin.gd
+++ addons/LineRenderer/line_plugin.gd
@@ -1,10 +1,9 @@
 @tool
 extends EditorPlugin
 
-
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	add_custom_type("Line Renderer 3d", "LineRenderer", preload("line_renderer.gd"), preload("line_render_icon.svg"))
+	add_custom_type("LineRenderer3D", "MeshInstance3D", preload("line_renderer.gd"), preload("line_render_icon.svg"))
 
 
 func _exit_tree():
