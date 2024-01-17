94001c1c84212cbc7dd95deb978f2da55f6ff74d
betalars <contact@betalars.de>
2024-01-17 19:34:43 +0100

    moving files plugin structure

---
+++ addons/LineRenderer/line_plugin.gd
@@ -0,0 +1,12 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree():
+	# Initialization of the plugin goes here.
+	add_custom_type("Line Renderer 3d", "LineRenderer", preload("line_renderer.gd"), preload("line_render_icon.svg"))
+
+
+func _exit_tree():
+	# Clean-up of the plugin goes here.
+	remove_custom_type("Line Renderer 3D")
